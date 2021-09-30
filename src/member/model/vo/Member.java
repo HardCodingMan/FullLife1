84a00b4c9f972e1029d319b90526ddec95fc1209
hardcodingman <xjzlfhrkwk@gmail.com>
2021-09-30 23:24:28 +0900

    Revert "member 업데이트"
    This reverts commit 1129ec688027211ca606e90f311a24a50350c2cd.

--- src/member/model/vo/Member.java
+++ src/member/model/vo/Member.java
@@ -34,19 +34,7 @@ public class Member {
       this.totalPoint = totalPoint;
    }
    
-   public Member(String userId, String userPwd, String userName, String userZumin, String userPhone, String userAddr,
-		String userEmail) {
-	super();
-	this.userId = userId;
-	this.userPwd = userPwd;
-	this.userName = userName;
-	this.userZumin = userZumin;
-	this.userPhone = userPhone;
-	this.userAddr = userAddr;
-	this.userEmail = userEmail;
-}
-
-public String getUserId() {
+   public String getUserId() {
       return userId;
    }
 
