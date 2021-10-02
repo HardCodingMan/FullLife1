1e3ab3985d5e33a1ff3b483d45ae5ff7b5f07846
찌미꼬미넷북\kang0 <stitchkang901128@gmail.com>
2021-10-02 15:25:20 +0900

    받지마시오

--- src/mypageHistory/service/HistoryService.java
+++ src/mypageHistory/service/HistoryService.java
@@ -2,14 +2,8 @@ package mypageHistory.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
-
 import common.JDBCTemplate;
-import manager.member.model.dao.M_memberDAO;
-import manager.member.moder.vo.M_PageData;
-import manager.member.moder.vo.M_member;
 import mypageHistory.dao.HistoryDAO;
-import mypageHistory.model.vo.History;
 import mypageHistory.model.vo.HistoryPage;
 
 public class HistoryService {
