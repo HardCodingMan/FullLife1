b3a7e139a4285fcf944c479c1a0ee6ade1760742
DESKTOP-5759EEA\iccon <dlthfal93@hanmail.net>
2021-10-04 09:34:08 +0900

    관리자 신청게시판 구현!

---
+++ src/manager/apply/model/vo/M_applyReply.java
@@ -0,0 +1,63 @@
+package manager.apply.model.vo;
+
+import java.util.Date;
+
+public class M_applyReply {
+	
+	private int apReNo;
+	private String reUserId;
+	private String apReCon;
+	private Date apReDate;
+	private int notiNo;
+	private int countRe;
+	
+	public M_applyReply() {}
+	
+	public int getApReNo() {
+		return apReNo;
+	}
+	public void setApReNo(int apReNo) {
+		this.apReNo = apReNo;
+	}
+	public String getReUserId() {
+		return reUserId;
+	}
+	public void setReUserId(String reUserId) {
+		this.reUserId = reUserId;
+	}
+	public String getApReCon() {
+		return apReCon;
+	}
+	public void setApReCon(String apReCon) {
+		this.apReCon = apReCon;
+	}
+	public Date getApReDate() {
+		return apReDate;
+	}
+	public void setApReDate(Date apReDate) {
+		this.apReDate = apReDate;
+	}
+	public int getNotiNo() {
+		return notiNo;
+	}
+	public void setNotiNo(int notiNo) {
+		this.notiNo = notiNo;
+	}
+	public int getCountRe() {
+		return countRe;
+	}
+	public void setCountRe(int countRe) {
+		this.countRe = countRe;
+	}
+
+	@Override
+	public String toString() {
+		return "M_applyReply [apReNo=" + apReNo + ", userId=" + reUserId + ", apReCon=" + apReCon + ", apReDate="
+				+ apReDate + ", notiNo=" + notiNo + ", countRe=" + countRe + "]";
+	}
+	
+	
+	
+	
+
+}
